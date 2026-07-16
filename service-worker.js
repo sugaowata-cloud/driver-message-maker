4d460dafba5708f3de5fe19f92aff9320686c2be
sugaowata-cloud <suga.owata@gmail.com>
2026-07-17 03:45:18 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'driver-message-maker-v5';
+const CACHE_NAME = 'driver-message-maker-v6';
 const APP_FILES = ['./', './index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
